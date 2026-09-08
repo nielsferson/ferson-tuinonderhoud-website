7dd6e9c30fbe2b5d9718a58a949b035c740b12c3
nielsfrsn <niels.ferson@hotmail.com>
2026-09-08 14:34:37 +0200

    Voeg melding 'onder constructie' toe aan OverOns-pagina

--- src/pages/OverOns.jsx
+++ src/pages/OverOns.jsx
@@ -4,6 +4,11 @@ export default function OverOns() {
       <div className="page-header">
         <h2>Over ons</h2>
         <p>Ferson Tuinonderhoud is een lokale tuinonderhoudszaak, actief in heel België.</p>
+
+        <br />
+        <br />
+
+        <p>Let op, deze pagina is nog onder constructie. Mogelijks is niet alles juist wat u nu ziet.</p>
       </div>
     </section>
   )
